85340f8b06c6f6b5984758810df474a6020d0886
Tatiana Kartavykh <shirkalinata@mail.ru>
2023-07-16 20:03:09 +0300

    done

---
+++ src/helpers/index.js
@@ -0,0 +1,4 @@
+export { addOffset } from "./add-offset";
+export { validateIp } from "./validate-ip";
+export { getAdrdess } from "./get-address";
+export { addTileLayer } from "./add-tyle-layer";
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import "babel-polyfill";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { validateIp, addTileLayer, getAdrdess, addOffset } from "./helpers";
+import icon from "../images/icon-location.svg";
+
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector("button");
+
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timezoneInfo = document.querySelector("#timezone");
+const ispInfo = document.querySelector("#isp");
+
+const markerIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [30, 40],
+});
+
+btn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+
+const mapArea = document.querySelector(".map");
+const map = L.map(mapArea).setView([51.505, -0.09], 13);
+addTileLayer(map);
+L.marker([51.505, -0.09], { icon: markerIcon }).addTo(map);
+
+function getData() {
+  if (validateIp(ipInput.value)) {
+    getAdrdess(ipInput.value).then(setInfo);
+  }
+}
+
+function handleKey(e) {
+  if (e.key === "Enter") {
+    getData();
+  }
+}
+
+function setInfo(mapData) {
+  console.log(mapData);
+  const { lat, lng, country, region, timezone } = mapData.location;
+
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText = country + " " + region;
+  timezoneInfo.innerText = timezone;
+  ispInfo.innerText = mapData.isp;
+
+  map.setView([lat, lng]);
+  L.marker([lat, lng], { icon: markerIcon }).addTo(map);
+
+  if (matchMedia("(max-width: 600px)").matches) {
+    addOffset(map);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getAdrdess("121.12.123.44").then(setInfo);
+});
