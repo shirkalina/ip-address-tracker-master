0abf48864876ad4ceaed6854ab30497ab201f4c4
Tatiana Kartavykh <shirkalinata@mail.ru>
2023-07-17 12:05:29 +0300

    new files

--- src/index.js
+++ src/index.js
@@ -1,64 +1,58 @@
-import 'babel-polyfill';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import { addOffset, addTileLayer, getAddress, validatIp} from './helpers';
-import icon from '../images/icon-location.svg';
+import "babel-polyfill";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { validateIp, addTileLayer, getAdrdess, addOffset } from "./helpers";
+import icon from "../images/icon-location.svg";
 
-const ipInput = document.querySelector('.search-bar__input');
-const btn = document.querySelector('button');
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector("button");
 
-const ipInfo = document.querySelector('#ip');
-const locationInfo = document.querySelector('#location');
-const timezoneInfo = document.querySelector('#timezone');
-const ispInfo = document.querySelector('#isp');
-
-btn.addEventListener('click', getData);
-ipInput.addEventListener('keydown', handleKey);
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timezoneInfo = document.querySelector("#timezone");
+const ispInfo = document.querySelector("#isp");
 
 const markerIcon = L.icon({
-    iconUrl: icon,
-    iconSize: [30, 40],
+  iconUrl: icon,
+  iconSize: [30, 40],
 });
 
-const mapArea = document.querySelector('.map');
-const map = L.map(mapArea, {
-    center: [51.505, -0.09],
-    zoom: 13,
-    zoomControl: false,
-});
+btn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+
+const mapArea = document.querySelector(".map");
+const map = L.map(mapArea).setView([51.505, -0.09], 13);
 addTileLayer(map);
-L.marker([51.505, -0.09], {icon: markerIcon}).addTo(map);
-
+L.marker([51.505, -0.09], { icon: markerIcon }).addTo(map);
 
 function getData() {
-    if (validatIp(ipInput.value)) {
-        getAddress(ipInput.value)
-            .then(setInfo)
-    }
+  if (validateIp(ipInput.value)) {
+    getAdrdess(ipInput.value).then(setInfo);
+  }
 }
 
 function handleKey(e) {
-    if (e.key === 'Enter') {
-        getData();
-    }
+  if (e.key === "Enter") {
+    getData();
+  }
 }
 
 function setInfo(mapData) {
-    const {lat, lng, country, region, timezone} = mapData.location;
+  const { lat, lng, country, region, timezone } = mapData.location;
 
-    ipInfo.innerText = mapData.ip;
-    locationInfo.innerText = country + ' ' + region;
-    timezoneInfo.innerText = timezone;
-    ispInfo.innerText = mapData.isp;
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText = country + " " + region;
+  timezoneInfo.innerText = timezone;
+  ispInfo.innerText = mapData.isp;
 
-    map.setView([lat, lng]);
-    L.marker([lat, lng], {icon: markerIcon}).addTo(map);
+  map.setView([lat, lng]);
+  L.marker([lat, lng], { icon: markerIcon }).addTo(map);
 
-    if (matchMedia("(max-width: 1023px)").matches) {
-        addOffset(map);
-    }
+  if (matchMedia("(max-width: 600px)").matches) {
+    addOffset(map);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    getAddress('102.22.22.1').then(setInfo)
+document.addEventListener("DOMContentLoaded", () => {
+  getAdrdess("121.12.123.44").then(setInfo);
 });
